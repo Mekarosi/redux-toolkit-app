4a9aa065a12d47333d4a1d38ed316267e07ab3fc
Mekarosi <mekarosi_assa@yahoo.com>
2023-06-06 07:38:26 -0700

    Added modal

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import CartContainer from "./component/CartContainer";
 import { useDispatch, useSelector } from "react-redux";
 import { calculateTotals } from "./features/cart/cartSlice";
 import { useEffect } from "react";
+import Modal from "./component/Modal";
 
 function App() {
   const { cartItems } = useSelector((store) => store.cart)
@@ -13,6 +14,7 @@ function App() {
   }, [cartItems])
   return (
   <main>
+    <Modal />
     <Navbar />
     <CartContainer />
   </main>);
